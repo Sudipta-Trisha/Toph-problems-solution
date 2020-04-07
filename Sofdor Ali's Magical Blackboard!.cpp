81db66d0a21c4f9bb92432449e41b3a53711d4ba
Sudipta Banik Trisha <43735253+Sudipta-Trisha@users.noreply.github.com>
2020-04-07 00:45:31 -0700

    Basic math

---
+++ Sofdor Ali's Magical Blackboard!.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long int 
+using namespace std;
+
+int main()
+{
+    ll test;
+    cin>>test;
+    while(test--){
+        ll a,b;
+        cin>>a>>b;
+        
+        cout<<a*b<<endl;
+    }
+    return 0;
+}
