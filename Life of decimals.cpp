cd06c096e1aeed19d4f646a9273ce475a6bd1e24
Sudipta Banik Trisha <43735253+Sudipta-Trisha@users.noreply.github.com>
2020-04-10 12:10:16 +0600

    Basic string implementation

---
+++ Life of decimals.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+string pii = {"14159265358979"};
+int main()
+{
+  int test;
+  cin>>test;
+  while (test--){
+    ll n;
+    cin>>n;
+    cout<<pii[n-1]<<endl;
+  }
+  return 0;
+}
